31dec938a5bf19190a2ac5d9904f93a1e9c15c4b
Duc Nguyen <ducgaogao4213@gmail.com>
2020-02-06 16:18:33 +0700

    Fix crash when add guest

--- app/screens/Play/subv2/add/guest.js
+++ app/screens/Play/subv2/add/guest.js
@@ -77,15 +77,16 @@ export default React.memo(() => {
 
     if (where === "C") {
       gameData.playerC = {
-        name: firstName + "." + lastName,
+        name: firstName,
+        lastName,
         email,
         index
       }
     }
     else if (where === "D") {
       gameData.playerD = {
-        name: firstName + "." + lastName,
-        firstName,
+        name: firstName,
+        lastName,
         email,
         index
       }
